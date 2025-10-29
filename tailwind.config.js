f03fe6da7404afb537a8b4a609098d71bccad068
PhoenixDispatchSolutions <dispatch@riseandhaul.com>
2025-10-29 04:40:59 -0700

    Update tailwind.config.js
    clean up tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,16 +6,6 @@ export default {
   ],
   theme: {
     extend: {
-      keyframes: {
-        phoenixPulse: {
-          '0%':   { backgroundPosition: '0% 50%', filter: 'brightness(1)' },
-          '50%':  { backgroundPosition: '100% 50%', filter: 'brightness(1.2)' },
-          '100%': { backgroundPosition: '0% 50%', filter: 'brightness(1)' },
-        },
-      },
-      animation: {
-        phoenixPulse: 'phoenixPulse 8s ease-in-out infinite',
-      },
       backgroundSize: {
         '200%': '200% 200%',
       },
@@ -23,4 +13,5 @@ export default {
   },
   plugins: [],
 };
+
 
